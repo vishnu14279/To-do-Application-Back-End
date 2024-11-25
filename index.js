286c7f3f8e613edc977b41a762887e0b318acc66
VISHNU PRIYAN M <80460869+vishnu14279@users.noreply.github.com>
2024-11-25 14:33:41 +0530

    Update index.js
    prod url changed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://to-do-application-front-end.onrender.com",
     methods: ["GET", "POST"],
     credentials: true
   }
